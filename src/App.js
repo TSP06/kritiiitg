f34eae3de6443f9860d23b46925e5a9f54d9d8d0
BharshitaDeka12 <bharshita2005@gmail.com>
2024-12-07 20:56:01 +0530

    Added Medium Prep

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import Header from './components/Header';
 import Cards from './components/Cards';
 import About from './About';
 import Navbar from './Navbar';
-
+import Heading from './mediumprep/Heading';
+import Card1 from './mediumprep/Card1';
+import Icon from './assets/Download.png';
 function App() {
   const pscard = [
     {
@@ -28,6 +30,33 @@ function App() {
       buttonText: 'See PS',
     },
   ]
+  const mpcard =[
+    {title: 'Software Devlopment',
+      description1:'by Coding Club',
+      description2:'Deadline : 24/2/2025',      
+      buttonText:'Register',
+      icon: Icon,
+    },
+    {title: 'Software Devlopment',
+      description1:'by Coding Club',
+      description2:'Deadline : 24/2/2025',
+      buttonText:'Register',
+      icon: Icon,
+    },
+    {title: 'Software Devlopment',
+      description1:'by Coding Club',
+      description2:'Deadline : 24/2/2025',
+      buttonText:'Register',
+      icon: Icon,
+    },
+    {title: 'Software Devlopment',
+      description1:'by Coding Club',
+      description2:'Deadline : 24/2/2025',
+      buttonText:'Register',
+      icon: Icon,
+    },
+    
+  ]
   return (
     <div className="App">
       <Navbar/>
@@ -44,6 +73,22 @@ function App() {
           />
         ))}
       </div>
+      <Heading/>
+      <div className="card-grid">
+        {mpcard.map((card,index) => (
+          <Card1
+          key={index}
+          title = {card.title}
+          icon = {card.icon}
+          description1 = {card.description1}
+          description2 = {card.description2}
+          buttonText = {card.buttonText}
+          />
+        ))}
+      </div>
+ 
+
+
     </div>
   );
 }

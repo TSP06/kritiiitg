d99272c378a5ab2e2f4d2e1c6d1320db88b8f894
TSP06 <tanusiwach2005@gmail.com>
2025-01-09 22:47:14 +0530

    done

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -3,7 +3,7 @@ const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  username: { type: String, required: true, unique: true },  // Changed from email to username
+  username: { type: String, required: true },  // Changed from email to username
   password: { type: String, required: true },
   role: { type: String, enum: ['admin', 'user'], default: 'user' },
 });
